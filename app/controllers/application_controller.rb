1c5f79a8bd7f402cf3a597fb0ef5e88af6fb9a36
[DN_OE18_RuBy]Le Thi Ngoc Tram <tramle7337@gmail.com>
2018-12-27 00:48:32 +0700

    signUp_signIn_signOut and Manage Users for Admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  include SessionsHelper
+
+  private
+
+  def logged_in_user
+    return if logged_in?
+    flash[:danger] = t ".login"
+    redirect_to login_path
+  end
 end
